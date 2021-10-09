2b8f24e418de62b4e288220c092c2e21780cef1d
unclejet <unclejet@126.com>
2021-10-09 16:24:09 +0800

    finished.

---
+++ spring-cloud-study/spring-cloud-gateway-quickstart/src/test/java/com/uj/study/gateway/ApplicationTest.java
@@ -0,0 +1,54 @@
+package com.uj.study.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author ：UncleJet
+ * @date ：Created in 2021/10/9 下午4:04
+ * @description：
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {"httpbin=http://localhost:${wiremock.server.port}"})
+@AutoConfigureWireMock(port = 0)
+public class ApplicationTest {
+
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test
+    public void contextLoads() throws Exception {
+        //Stubs
+        stubFor(get(urlEqualTo("/get"))
+                .willReturn(aResponse()
+                        .withBody("{\"headers\":{\"Hello\":\"World\"}}")
+                        .withHeader("Content-Type", "application/json")));
+        stubFor(get(urlEqualTo("/delay/3"))
+                .willReturn(aResponse()
+                        .withBody("no fallback")
+                        .withFixedDelay(3000)));
+
+        webClient
+                .get().uri("/get")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.headers.Hello").isEqualTo("World");
+
+        webClient
+                .get().uri("/delay/3")
+                .header("Host", "www.circuitbreaker.com")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(
+                        response -> assertThat(response.getResponseBody()).isEqualTo("fallback".getBytes()));
+    }
+}
